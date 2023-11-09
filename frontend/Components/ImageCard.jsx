edcdd5b686243da6eee28efba94930d16d581548
pratiushp <pratiushprasain@gmail.com>
2023-11-09 14:32:42 +0545

    Product Image Screen

---
+++ frontend/Components/ImageCard.jsx
@@ -0,0 +1,39 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { colors, defaultStyle } from "../Styles/style";
+import { Avatar } from "react-native-paper";
+
+const ImageCard = ({ src, id, deleteHandler }) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: src }}
+        style={{
+          width: "100%",
+          height: "80%",
+          resizeMode: "contain",
+        }}
+      />
+      <TouchableOpacity onPress={() => deleteHandler(id)}>
+        <Avatar.Icon
+          icon={"delete"}
+          size={30}
+          style={{ backgroundColor: colors.color1 }}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.color2,
+    elevation: 5,
+    margin: 10,
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 10,
+    height: 300,
+  },
+});
+export default ImageCard;
--- frontend/Screens/Admin/ProductImages.jsx
+++ frontend/Screens/Admin/ProductImages.jsx
@@ -1,10 +1,99 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { colors, defaultStyle, formHeading } from "../../Styles/style";
+import Header from "../../Components/Header";
+import ImageCard from "../../Components/ImageCard";
+import { Avatar, Button } from "react-native-paper";
 
-const ProductImages = () => {
+const ProductImages = ({ navigation, route }) => {
+  const [images] = useState(route.params.images);
+  const [productId] = useState(route.params.id);
+  const [image, setImage] = useState(null);
+  const [imageChanged, setImageChanged] = useState(false);
+
+  const loading = false;
+  const deleteHandler = (id) => {
+    console.log("image id", id);
+    console.log("Product Id", productId);
+  };
+
+  const submitHandler = () => {};
   return (
-    <View>
-      <Text>ProductImages</Text>
+    <View
+      style={{
+        ...defaultStyle,
+        backgroundColor: colors.color5,
+      }}>
+      <Header back={true} />
+      <View style={{ marginBottom: 20 }}>
+        <Text style={formHeading}>Product Images</Text>
+      </View>
+
+      <ScrollView style={{ marginBottom: 20 }}>
+        <View
+          style={{
+            backgroundColor: colors.color2,
+            padding: 40,
+            minHeight: 400,
+          }}>
+          {images.map((i) => (
+            <ImageCard
+              src={i.url}
+              id={i.id}
+              key={i._id}
+              deleteHandler={deleteHandler}
+            />
+          ))}
+        </View>
+      </ScrollView>
+      <View
+        style={{
+          padding: 20,
+          borderRadius: 10,
+          backgroundColor: colors.color3,
+        }}>
+        <Image
+          style={{
+            backgroundColor: colors.color2,
+            width: 100,
+            height: 100,
+            alignSelf: "center",
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: image,
+          }}
+        />
+
+        <View
+          style={{ flexDirection: "row", justifyContent: "center", margin: 5 }}>
+          <TouchableOpacity
+            activeOpacity={0.9}
+            onPress={() =>
+              navigation.navigate("camera", { updateProduct: true })
+            }>
+            <Avatar.Icon
+              icon={"camera"}
+              size={30}
+              style={{
+                backgroundColor: colors.color2,
+              }}
+              color={colors.color3}
+            />
+          </TouchableOpacity>
+        </View>
+        <Button
+          style={{
+            backgroundColor: colors.color1,
+            padding: 6,
+          }}
+          textColor={colors.color2}
+          loading={loading}
+          onPress={submitHandler}
+          disabled={!imageChanged}>
+          Add
+        </Button>
+      </View>
     </View>
   );
 };
