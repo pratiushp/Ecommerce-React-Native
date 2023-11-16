c71c9bcbb6af91b44a2eeda0f404db95f7d7ff77
pratiushp <pratiushprasain@gmail.com>
2023-11-16 16:18:28 +0545

    Reducer Implement

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./reducers/userReducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
 export const server = "http://localhost:5000";
